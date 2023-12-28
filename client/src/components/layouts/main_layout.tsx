ec647dc45f3e93c9d75d62285a674526e88dc1c1
netcam22 <annettelesage@btinternet.com>
2023-12-28 09:22:53 +0000

    feat: implement logo

--- client/src/components/layouts/main_layout.tsx
+++ client/src/components/layouts/main_layout.tsx
@@ -1,11 +1,12 @@
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import { Outlet } from 'react-router-dom';
+import logo from './../../assets/images/logo.png';
 
 const MainLayout = () => {
     return (
     <>
-    <Header/>
+    <Header image = {logo} />
      <main className = "page">
         <Outlet/>
      </main>
