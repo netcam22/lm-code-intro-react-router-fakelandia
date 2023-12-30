ccaa75be25a185931f6a92ba5074407f37eac25b
netcam22 <annettelesage@btinternet.com>
2023-12-30 19:14:46 +0000

    docs: add types: [node, @testing-library/jest-dom] to complier options in ts.config

--- client/src/components/form-components/error-message.test.tsx
+++ client/src/components/form-components/error-message.test.tsx
@@ -1,11 +1,16 @@
-import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { it, expect} from 'vitest';
+import { ErrorMessage, ErrorMessageProps } from './error-message';
+import "@testing-library/jest-dom/extend-expect";
 
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
-  });
+it('will display message when passed through props', async () => {
 
-  it('false to be false', () => {
-    expect(false).toBe(false);
-  });
+	const requiredProps: ErrorMessageProps = {
+		message: "must be between 10 and 500 characters"
+	}
+
+	render(<ErrorMessage {...requiredProps}/>);
+	
+	const characterElement = await screen.findByText(/must be between 10 and 500 characters/i);
+    expect(characterElement).toBeInTheDocument();
 });
