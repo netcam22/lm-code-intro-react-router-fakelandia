95e71dc717036aac6c558eea226787267b97f373
netcam22 <annettelesage@btinternet.com>
2024-01-01 13:57:29 +0000

    feat: implement test to display select option text

--- client/src/components/form-components/select-option.test.tsx
+++ client/src/components/form-components/select-option.test.tsx
@@ -2,15 +2,28 @@ import { render, screen } from '@testing-library/react';
 import { it, expect} from 'vitest';
 import { SelectOption, SelectOptionProps } from './select-option';
 
-it('Option for select element is displayed', () => {
+it('displays option for select element', () => {
 
 	const requiredProps: SelectOptionProps = {
 		option: "I just want to talk",
-	    optionValue: "just-talk"
+        optionValue: "just-talk"
 	}
 
 	render(<SelectOption {...requiredProps}/>);
 	
 	const option = screen.getByRole("option");
     expect(option).toBeInTheDocument();
+});
+
+it('displays option I just want to talk when form is rendered', () => {
+
+	const requiredProps: SelectOptionProps = {
+		option: "I just want to talk",
+        optionValue: "just-talk"
+	}
+
+	render(<SelectOption {...requiredProps}/>);
+	
+	const option = screen.getByRole("option");
+    expect(option.innerHTML).toBe("I just want to talk");
 });
