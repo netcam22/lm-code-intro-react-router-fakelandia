9defdc76748b7e1a53dffaf32d16f7ebe8916c6e
netcam22 <annettelesage@btinternet.com>
2024-01-01 13:50:17 +0000

    feat: implement test for select input value

--- client/src/components/form-components/select-input.test.tsx
+++ client/src/components/form-components/select-input.test.tsx
@@ -38,3 +38,22 @@ it('displays select input field when rendering confession form', () => {
 	//Assert
     expect(selectInput).toBeInTheDocument();
 });
+
+it('Reason for contact input field displays value pvegetables', () => {
+    //Arrange
+	const requiredProps : SelectInputProps = {
+		title: "Reason for contact",
+		role: "reason",
+		value: "vegetables",
+		onChange: () => {},
+		errorMessage: "",
+		attempted: false,
+		options: MISDEMEANOUR_FORM_OPTIONS, 
+        optionValues: MISDEMEANOUR_FORM_OPTION_VALUES
+	};
+	//Act
+	render(<SelectInput {...requiredProps}/>);
+	const selectInput: HTMLSelectElement = screen.getByLabelText("Reason for contact");
+	//Assert
+	expect(selectInput.value).toBe("vegetables");
+});
