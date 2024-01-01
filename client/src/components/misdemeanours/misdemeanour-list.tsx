b6bee216f2b1b47f8c811f40d1114aecb5148046
netcam22 <annettelesage@btinternet.com>
2024-01-01 08:21:26 +0000

    fix: remove loading cuplication

--- client/src/components/misdemeanours/misdemeanour-list.tsx
+++ client/src/components/misdemeanours/misdemeanour-list.tsx
@@ -1,20 +1,16 @@
 import MisdemeanourItem from "./misdemeanour-item";
-import { MisdemeanourObject } from "../../types/misdemeanour_client_types";
-import ShowLoading from "../loading/show-loading";
 import { useContext } from "react";
 import { MisdemeanourFilterContext } from "./misdemeanour-container";
+import { MisdemeanourObject } from "../../types/misdemeanour_client_types";
 import { MisdemeanourContext } from "./misdemeanour-data-wrapper";
 
 const MisdemeanourList : React.FC = () => {
 
 const [misdemeanourData] =  useContext(MisdemeanourContext);
-
 const [selectedFilter] = useContext(MisdemeanourFilterContext);
 
 return (
         <>
-        {misdemeanourData && misdemeanourData.length === 0 && 
-	<ShowLoading /> }
 
         {misdemeanourData && selectedFilter && selectedFilter.filterMisdemeanours === "all" &&
         misdemeanourData.map((item: MisdemeanourObject) =>
