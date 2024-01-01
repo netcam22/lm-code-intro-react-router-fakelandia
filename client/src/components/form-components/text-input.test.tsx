c0c241a4ab871f01ca5e09409fe94234a6f25f5c
netcam22 <annettelesage@btinternet.com>
2024-01-01 12:50:08 +0000

    feat: implement input field test

--- client/src/components/form-components/text-input.test.tsx
+++ client/src/components/form-components/text-input.test.tsx
@@ -39,3 +39,20 @@ it('renders subject input field', () => {
 	//Assert
     expect(inputField).toBeInTheDocument();
 });
+
+it('displays My Confession in input field when typed by user', () => {
+    //Arrange
+	const requiredProps: TextInputProps = {
+		title: "Subject",
+        role: "subject",
+        value: "My Confession",
+        onChange: () => {},
+        errorMessage: "",
+        attempted: false
+	};
+	//Act
+	render(<TextInput {...requiredProps}/>);
+	const inputField: HTMLInputElement = screen.getByLabelText("Subject");
+	//Assert
+	expect(inputField.value).toBe("My Confession");
+});
