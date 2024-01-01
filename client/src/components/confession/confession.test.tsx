0dbcd12f9801daed20cb52de3c46bf5dedebd330
netcam22 <annettelesage@btinternet.com>
2024-01-01 10:57:06 +0000

    feat: implement misdemeanour title test

--- client/src/components/confession/confession.test.tsx
+++ client/src/components/confession/confession.test.tsx
@@ -3,13 +3,13 @@ import { it, expect} from 'vitest';
 import ConfessionForm from './confession-form';
 import Confession from './confession';
 
-it('renders confession title', () => {
+it('renders confession title when confession page loads', () => {
 	render(<Confession />);
 	const confessionTitle = screen.getByText("Confess to Us");
 	expect(confessionTitle).toBeInTheDocument();
 });
 
-it('renders form element', () => {
+it('renders form element when confession page loads', () => {
 	const { container } = render(<ConfessionForm />);
 	expect(container.firstChild).toHaveClass('form');
 });
