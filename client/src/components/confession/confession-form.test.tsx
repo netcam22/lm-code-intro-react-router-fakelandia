e8b14e3e5b053c255c16ea0428496071240dc765
netcam22 <annettelesage@btinternet.com>
2024-01-01 21:36:23 +0000

    feat: implement test for change option in select input

--- client/src/components/confession/confession-form.test.tsx
+++ client/src/components/confession/confession-form.test.tsx
@@ -1,17 +1,11 @@
-import { fireEvent, render, screen} from '@testing-library/react';
-import { it, expect, vi} from 'vitest';
+import { render, screen} from '@testing-library/react';
+import { it, expect} from 'vitest';
 import ConfessionForm from './confession-form';
 
-it('calls the handleSubmit function when the confess button is clicked', async () => {
-
+it('calls the handleSubmit function when the form is submitted', async () => {
 	render(<ConfessionForm />);
 	
 	const submitButton = screen.getByRole("submitConfessionButton");
     expect(submitButton).toBeInTheDocument();
-/*
-	if (submitButton) {
-		await userEvent.click(submitButton);
-	}
-	expect(mockSubmit).toBeCalled();
-*/
+
 });
