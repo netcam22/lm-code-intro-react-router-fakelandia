24121034635c4d8e5b65a1566f4227d09dd2d787
netcam22 <annettelesage@btinternet.com>
2024-01-01 13:18:13 +0000

    feat: implement error message test for invalid text input

--- client/src/components/form-components/text-input.test.tsx
+++ client/src/components/form-components/text-input.test.tsx
@@ -3,7 +3,6 @@ import { it, expect} from 'vitest';
 import { TextInput } from './text-input';
 import { TextInputProps } from "./text-input";
 
-
 it('renders form label for subject', () => {
     //Arrange
 const requiredProps: TextInputProps = {
@@ -55,4 +54,23 @@ it('displays My Confession in input field when typed by user', () => {
 	const inputField: HTMLInputElement = screen.getByLabelText("Subject");
 	//Assert
 	expect(inputField.value).toBe("My Confession");
+});
+
+it('displays error message under input field if confession attempted and value is invalid', () => {
+    //Arrange;
+	const requiredProps = {
+		title: "Subject",
+        role: "subject",
+        value: "M",
+        onChange: () => {},
+        errorMessage: "must be between 2 and 20 characters",
+        attempted: true
+	};
+	render(<TextInput {...requiredProps}/>);
+	//Act
+	const message = screen.getByText(
+		/must be between 2 and 20 characters/i
+	);
+	//Assert
+	expect(message).toBeInTheDocument();
 });
