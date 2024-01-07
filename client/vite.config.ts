45decfd9762099ea07f7490d2ffbbf9c9a2ad8f0
netcam22 <annettelesage@btinternet.com>
2024-01-07 22:27:47 +0000

    feat: update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/lm-code-intro-react-router-fakelandia/",
   test: {
     globals: true,
     environment: 'jsdom',
