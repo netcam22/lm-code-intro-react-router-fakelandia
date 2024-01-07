40d9beff4a8a071011431ee08b00f7e64826f856
netcam22 <annettelesage@btinternet.com>
2024-01-07 13:30:33 +0000

    feat: update nav menu styling

--- client/src/components/header/nav.tsx
+++ client/src/components/header/nav.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Nav = () => <nav className = "navbar">
     <span className = "navbar__element">
     <span className = "navbar__element--bars"><i className="fa fa-bars"></i></span>
-    <ul className = "navbar__list">
+    <ul className = "navbar__list navbar__list--light">
         <li className = "navbar__item navbar__item--dark"><NavLink to = "/">Home</NavLink></li>
         <li className = "navbar__item navbar__item--dark"><NavLink to = "/misdemeanours">Misdemeanours</NavLink></li>
         <li className = "navbar__item navbar__item--dark"><NavLink to = "/confession">Confess To Us</NavLink>
