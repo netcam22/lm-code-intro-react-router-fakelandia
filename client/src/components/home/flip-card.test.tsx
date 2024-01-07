3b7575b1a6d4e537583e079038d06a907f4415d6
netcam22 <annettelesage@btinternet.com>
2024-01-07 20:41:39 +0000

    feat: implement reported today test

--- client/src/components/home/flip-card.test.tsx
+++ client/src/components/home/flip-card.test.tsx
@@ -33,3 +33,19 @@ it('renders 5 as number of vegetable misdemeanours in flip card', () => {
     const misdemeanourCount = screen.getByText(/5/);
 	expect(misdemeanourCount).toBeInTheDocument();
 });
+
+it('renders words reported today as part of information in flip card', () => {
+
+    const requiredProps: FlipCardProps  = {
+        title: "Not Eating Your Vegetables",
+        count: 5,
+        image: "/src/assets/images/logo.png",
+        alt: "Picture of Not Eating Your Vegetables misdemeanour",
+        cssClassType: "vegetables",
+        icon: "🥗"
+    }
+
+	render(<FlipCard {...requiredProps}/>);
+    const reported = screen.getByText(/reported today/);
+	expect(reported).toBeInTheDocument();
+});
