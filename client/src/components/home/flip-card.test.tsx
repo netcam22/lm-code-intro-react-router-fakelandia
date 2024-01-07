376767e8f41439038eb34f27da6f8ad7b7cd7708
netcam22 <annettelesage@btinternet.com>
2024-01-07 18:58:23 +0000

    feat: implement date tests

---
+++ client/src/components/home/flip-card.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import { it, expect} from 'vitest';
+import Home from './home';
+
+it('renders home title', () => {
+	render(<Home />);
+	const homeTitle = screen.getByText(/Home/);
+	expect(homeTitle).toBeInTheDocument();
+});
